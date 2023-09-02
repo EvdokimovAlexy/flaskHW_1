6a4ba37ed86a6b59f73dd51c36aef0918ac4e556
Prometheus <poligraphich@gmail.com>
2023-09-03 00:08:35 +0300

    v2

--- HW1/main.py
+++ HW1/main.py
@@ -22,14 +22,9 @@ def clothes():
 @app.route('/jackets/')
 def jackets():
     jacket_list = [
-        {"photo": "https://stok-m.ru/image//fromnew/Z0720232820234320230201/image63da0ee47a1df.jpg",
-         "name": "кутка_1",
-         "cost": 5000
+        {
          },
-        {"photo": "https://stok-m.ru/image//fromnew/Z1220223120222420220509/image62790fd9c4021.jpg",
-         "name": "кутка_2",
-         "cost": 10000
-         }
+
     ]
     return render_template('jackets.html', jacket_list=jacket_list)
 
